f85a4b7f0047f993eb50fa470444fc96aa31ddd6
superhotfire140 <35212758+superhotfire140@users.noreply.github.com>
2019-03-06 23:14:30 -0800

    Update wowmarket.py

--- wowmarket.py
+++ wowmarket.py
@@ -1,14 +1,19 @@
-from wowapi import WowApi
-import pandas
+from wowapi import WowApi #https://python-wowapi.readthedocs.io/en/latest/modules/wowapi.html#wowapi.api.WowApi.get_auctions
+import pandas as pd
 import requests
+import numpy
 
-api = WowApi('2e5f247983b941ec8cea07f8e6e8802f', 'muq1xUmblMpnjMIZuRts60kmPFKCUObY')
+api = WowApi('PUBLIC_KEY', 'PRIVATE_KEY')
 api.get_token('us', namespace='dynamic-us', locale='en_US')
 foo = api.get_auctions('us', 'zuljin', locale='en_US')
 data = requests.get(foo['files'][0]['url'], 'json')
 
 auctions = data.json()['auctions']
 
-thaeleeaAuctions = list(filter(lambda auction: auction['owner'] == 'Thaeleea', auctions))
+df = pd.DataFrame(auctions)
+
+df['owner'].mode()
+
+thaeleeaAuctions = list(filter(lambda auction: auction['owner'] == 'Unhoarie', auctions))
 
 sum(map(lambda auction: auction['buyout'], thaeleeaAuctions))/10000
