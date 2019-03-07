ec67798b589295b11ab586fb29b1aa810f459b2c
superhotfire140 <35212758+superhotfire140@users.noreply.github.com>
2019-03-06 21:08:45 -0800

    Update wowmarket.py
    Added request from the url returned by blizz

--- wowmarket.py
+++ wowmarket.py
@@ -1,15 +1,14 @@
 from wowapi import WowApi
 import pandas
+import requests
 
-api = WowApi('PUBLIC_KEY', 'PRIVATE_KEY')
+api = WowApi('2e5f247983b941ec8cea07f8e6e8802f', 'muq1xUmblMpnjMIZuRts60kmPFKCUObY')
 api.get_token('us', namespace='dynamic-us', locale='en_US')
 foo = api.get_auctions('us', 'zuljin', locale='en_US')
-foo.json().keys()
+data = requests.get(foo['files'][0]['url'], 'json')
 
-foo.json()['auctions']
+auctions = data.json()['auctions']
 
-auctions = foo.json()['auctions']
 thaeleeaAuctions = list(filter(lambda auction: auction['owner'] == 'Thaeleea', auctions))
-
 
 sum(map(lambda auction: auction['buyout'], thaeleeaAuctions))/10000
