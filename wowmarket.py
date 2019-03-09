5116bcb10d054ef97a3e332e5be27f0dda8f1894
superhotfire140 <35212758+superhotfire140@users.noreply.github.com>
2019-03-08 17:46:12 -0800

    Fixes

--- wowmarket.py
+++ wowmarket.py
@@ -1,9 +1,9 @@
 from wowapi import WowApi #https://python-wowapi.readthedocs.io/en/latest/modules/wowapi.html#wowapi.api.WowApi.get_auctions
 import pandas as pd
 import requests
-import openpyxl
 
-api = WowApi('2e5f247983b941ec8cea07f8e6e8802f', 'muq1xUmblMpnjMIZuRts60kmPFKCUObY')
+
+api = WowApi('PUBLIC_KEY', 'PRIVATE_KEY')
 api.get_token('us', namespace='dynamic-us', locale='en_US')
 foo = api.get_auctions('us', 'zuljin', locale='en_US')
 data = requests.get(foo['files'][0]['url'], 'json')
@@ -11,6 +11,7 @@ data = requests.get(foo['files'][0]['url'], 'json')
 auctions = data.json()['auctions']
 
 df = pd.DataFrame(auctions)
+df['buyouts'].transform(lambda x: x + 1)
 
 df['owner'].mode()
 
