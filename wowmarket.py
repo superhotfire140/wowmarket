372329b0c7e60344bf9638065bbf4ccda160e7d1
superhotfire140 <35212758+superhotfire140@users.noreply.github.com>
2019-03-10 16:26:31 -0700

    Working with some analysis
    Added Price/Unit, Price/Unit Avg and Std (based on 200 stack market), and one and two standard deviations below Market Avg.

--- wowmarket.py
+++ wowmarket.py
@@ -11,8 +11,35 @@ data = requests.get(foo['files'][0]['url'], 'json')
 
 auctions = data.json()['auctions']
 
-akunda = list(filter(lambda auction: auction['item'] == 152507, auctions))
+# # Akunda Data
+# akunda = list(filter(lambda auction: auction['item'] == 152507, auctions))
+# akunda_df = pd.DataFrame(akunda)
+# akunda_df['buyout'] = akunda_df['buyout']/10000
+# akunda_df['Price/Unit'] = akunda_df['buyout']/akunda_df['quantity']
 
-df = pd.DataFrame(akunda)
+# Riverbud Data
+riverbud = list(filter(lambda auction: auction['item'] == 152505, auctions))
+riverbud_df = pd.DataFrame(riverbud)
+riverbud_df['buyout'] = riverbud_df['buyout']/10000
+riverbud_df['Price/Unit'] = riverbud_df['buyout']/riverbud_df['quantity']
+riverbud_200_df = riverbud_df[riverbud_df['quantity'] == 200]
 
-df.to_excel('auctionhouse.xlsx', sheet_name='MarketInfo')
+# Remove Outliers
+riverbud_200_avg = riverbud_200_df['Price/Unit'].mean()
+riverbud_200_std = riverbud_200_df['Price/Unit'].std()
+
+riverbud_200_df = riverbud_200_df[riverbud_200_df['Price/Unit'] < riverbud_200_avg + 2*riverbud_200_std]
+riverbud_df = riverbud_df[riverbud_df['Price/Unit'] < riverbud_200_avg + 2*riverbud_200_std]
+
+# Get new avg and std
+riverbud_200_avg = riverbud_200_df['Price/Unit'].mean()
+riverbud_200_std = riverbud_200_df['Price/Unit'].std()
+
+riverbud_df['Price/Unit Avg'] = riverbud_200_avg
+riverbud_df['Price/Unit Std'] = riverbud_200_std
+riverbud_df['Std 1'] = riverbud_200_avg - riverbud_200_std
+riverbud_df['Std 2'] = riverbud_200_avg - riverbud_200_std*2
+riverbud_df['%'] = riverbud_df['Price/Unit'] / riverbud_200_avg
+
+# Write to Excel
+riverbud_df.to_excel('riverbud_auctions.xlsx', sheet_name='Riverbud')
