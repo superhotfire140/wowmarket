6d568aa5d102b23393183a8dc3d079cd4e197aaf
superhotfire140 <35212758+superhotfire140@users.noreply.github.com>
2019-03-11 18:54:19 -0700

    Changed to elim 1.5*std

--- wowmarket.py
+++ wowmarket.py
@@ -2,7 +2,9 @@ from wowapi import WowApi #https://python-wowapi.readthedocs.io/en/latest/module
 import pandas as pd
 import requests
 import config
+import datetime
 
+input_item = input("Item #: ")
 
 api = WowApi(config.PUBLIC_KEY, config.PRIVATE_KEY)
 api.get_token('us', namespace='dynamic-us', locale='en_US')
@@ -11,35 +13,37 @@ data = requests.get(foo['files'][0]['url'], 'json')
 
 auctions = data.json()['auctions']
 
-# # Akunda Data
-# akunda = list(filter(lambda auction: auction['item'] == 152507, auctions))
-# akunda_df = pd.DataFrame(akunda)
-# akunda_df['buyout'] = akunda_df['buyout']/10000
-# akunda_df['Price/Unit'] = akunda_df['buyout']/akunda_df['quantity']
-
-# Riverbud Data
-riverbud = list(filter(lambda auction: auction['item'] == 152505, auctions))
-riverbud_df = pd.DataFrame(riverbud)
-riverbud_df['buyout'] = riverbud_df['buyout']/10000
-riverbud_df['Price/Unit'] = riverbud_df['buyout']/riverbud_df['quantity']
-riverbud_200_df = riverbud_df[riverbud_df['quantity'] == 200]
+# market Data
+market = list(filter(lambda auction: auction['item'] == int(input_item), auctions))
+market_df = pd.DataFrame(market)
+market_df['buyout'] = market_df['buyout']/10000
+market_df['Price/Unit'] = market_df['buyout']/market_df['quantity']
+market_200_df = market_df[market_df['quantity'] == 200].copy()
 
 # Remove Outliers
-riverbud_200_avg = riverbud_200_df['Price/Unit'].mean()
-riverbud_200_std = riverbud_200_df['Price/Unit'].std()
+market_200_avg = market_200_df['Price/Unit'].mean()
+market_200_std = market_200_df['Price/Unit'].std()
 
-riverbud_200_df = riverbud_200_df[riverbud_200_df['Price/Unit'] < riverbud_200_avg + 2*riverbud_200_std]
-riverbud_df = riverbud_df[riverbud_df['Price/Unit'] < riverbud_200_avg + 2*riverbud_200_std]
+market_200_df = market_200_df[market_200_df['Price/Unit'] < market_200_avg + 1.5*market_200_std]
 
 # Get new avg and std
-riverbud_200_avg = riverbud_200_df['Price/Unit'].mean()
-riverbud_200_std = riverbud_200_df['Price/Unit'].std()
+market_200_avg = market_200_df['Price/Unit'].mean()
+market_200_std = market_200_df['Price/Unit'].std()
 
-riverbud_df['Price/Unit Avg'] = riverbud_200_avg
-riverbud_df['Price/Unit Std'] = riverbud_200_std
-riverbud_df['Std 1'] = riverbud_200_avg - riverbud_200_std
-riverbud_df['Std 2'] = riverbud_200_avg - riverbud_200_std*2
-riverbud_df['%'] = riverbud_df['Price/Unit'] / riverbud_200_avg
+market_df = market_df[market_df['Price/Unit'] < market_200_avg + 2*market_200_std]
+
+market_df['Price/Unit Avg'] = market_200_avg
+market_df['Price/Unit Std'] = market_200_std
+market_df['Std 1'] = market_200_avg - market_200_std
+market_df['Std 2'] = market_200_avg - market_200_std*2
+market_df['%'] = market_df['Price/Unit'] / market_200_avg
+
+#Write to Market Price file
+price_upper = market_200_avg + market_200_std*2
+price_lower = market_200_avg - market_200_std*2
+
+time_stamp = datetime.datetime.now()
+time_stamp = time_stamp.strftime("%Y-%m-%d %H:%M")
 
 # Write to Excel
-riverbud_df.to_excel('riverbud_auctions.xlsx', sheet_name='Riverbud')
+market_df.to_excel('market_auctions.xlsx', sheet_name='market')
